fbc4223f52b9f535a910be2d22567eb673754fd9
matspol <mateus.spoliantads@gmail.com>
2026-06-10 17:20:19 -0300

    build web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1421048915" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1358524533" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
